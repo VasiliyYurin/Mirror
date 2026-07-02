d9ec6f75eec38fa91e26f68863580eb240f35314
VasiliyYurin <vasliybrokeracc@gmail.com>
2026-07-02 19:57:26 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+/*
+========================================
+Mirror 2.0
+Application
+========================================
+*/
 
+window.addEventListener("load", async () => {
+
+    await startCamera();
+
+    if (!APP_CONFIG.text) return;
+
+    setTimeout(() => {
+
+        showMessage(APP_CONFIG.text);
+
+        setTimeout(() => {
+
+            hideMessage();
+
+        }, APP_CONFIG.duration);
+
+    }, APP_CONFIG.delay);
+
+});
